f5815518e75d49d402d750e1d76a4ea0387cf1f2
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-14 15:07:39 -0500

    started lab

---
+++ CountryListWithCollectionView/CountryListWithCollectionView/CountryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CountryCollectionViewCell.swift
+//  CountryListWithCollectionView
+//
+//  Created by Yuliia Engman on 1/14/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class CountryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var countryImage: UIImageView!
+    @IBOutlet weak var countryNameLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    
+    
+}
---
+++ CountryListWithCollectionView/CountryListWithCollectionView/CountryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryListWithCollectionView
+//
+//  Created by Yuliia Engman on 1/14/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class CountryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
